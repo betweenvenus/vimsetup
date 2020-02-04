c0e01fa2b1d0512f1dfd1d63de68000befe8c82e
Jon <Mopdog-1@mopdog-webdev.local>
2020-02-04 15:42:41 -0500

    init

---
+++ vimsetup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# NOTE: This script should be sourced ('source ./vimsetup.sh -i')
+# Arguments guide:
+# -i: *Installs* the vimrc. Only run this the first time. Clones, then sets path.
+# -e: Sets up your environment for the custom vimrc. Run this every time you SSH in.
+
+while getopts "ie" opt; do
+    case $opt in
+        i)
+            git clone https://github.com/betweenvenus/vimrc ~/html/vimrc
+            export VIMINIT='source $CUSTOMVIMRC'
+            export CUSTOMVIMRC='~/html/vimrc/.vimrc'
+            echo 'done!'
+            ;;
+        e)
+            export VIMINIT='source $CUSTOMVIMRC'
+            export CUSTOMVIMRC='~/html/vimrc/.vimrc'
+            echo 'ready!'
+            ;;
+        esac
+    done
+    shift "$(($OPTIND -1))"
